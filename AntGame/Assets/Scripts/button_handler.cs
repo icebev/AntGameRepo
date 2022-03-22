f5db865590c698f8e5e8577432d293442ed4245e
TwistyC <42874167+TwistedC@users.noreply.github.com>
2022-03-22 10:37:41 +0000

    making main menu
    +created temp button and stuff assets
    +started animating (cant continue due to bugged animator)

---
+++ AntGame/Assets/Scripts/button_handler.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class button_handler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GoBack() 
+    {
+    
+    }
+
+    void StartGame()
+    {
+
+    }
+
+    
+
+}
