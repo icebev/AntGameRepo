75c054ae4077acd1f4fb9fee88758ea801cc520d
MallyKM <malachikm1703@gmail.com>
2022-04-06 01:28:17 +0100

    ai works i think/hope

---
+++ Test/Assets/Scripts/Script.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script : MonoBehaviour
+{
+    private int number = 17;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(number);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
